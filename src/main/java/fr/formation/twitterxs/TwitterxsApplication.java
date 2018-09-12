97e7ec54d020bdf28e48227604bcf8db22f5229f
BDN <benoit.damblon@uca.fr>
2018-09-12 13:46:35 +0200

    Premier commit

---
+++ src/main/java/fr/formation/twitterxs/TwitterxsApplication.java
@@ -0,0 +1,12 @@
+package fr.formation.twitterxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterxsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TwitterxsApplication.class, args);
+  }
+}
